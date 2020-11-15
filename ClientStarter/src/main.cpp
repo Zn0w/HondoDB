12c701304629bc07ae13e1098e6a17ed12cb779b
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-11-15 18:56:57 +0300

    added test networking code that actually works!

--- ClientStarter/src/main.cpp
+++ ClientStarter/src/main.cpp
@@ -8,34 +8,43 @@
 
 #include <core/client/client.h>
 
+using namespace hondo::net;
 
-int main()
+using asio::ip::tcp;
+
+
+int main(int argc, char* argv[])
 {
-	hondo::Client client("daniil", "12345");
-	client.connect("127.0.0.1", 27575);
-
-	bool accepted = false;
-
-	if (client.is_connected())
+	try
 	{
-		if (accepted)
-		{
-			client.authenticate();
-		}
+		asio::io_context io_context;
 
-		if (!client.incoming().empty())
+		tcp::resolver resolver(io_context);
+		tcp::resolver::results_type endpoints =
+			resolver.resolve("127.0.0.1", "13");
+
+		tcp::socket socket(io_context);
+		asio::connect(socket, endpoints);
+
+		for (;;)
 		{
-			auto message = std::move(client.incoming().front().message);
-			client.incoming().pop_front();
-			switch (message.header.id)
-			{
-			case hondo::net::MessageType::ServerAccept:
-			{
-				std::cout << "Server Accepted Connection" << std::endl;
-				accepted = true;
-			} break;
-			}
+			char buf[256];
+			asio::error_code error;
+
+			size_t len = socket.read_some(asio::buffer(buf, 256), error);
+
+			if (error == asio::error::eof)
+				break; // Connection closed cleanly by peer.
+			else if (error)
+				throw asio::system_error(error); // Some other error.
+
+			std::cout << "Server: ";
+			std::cout.write(buf, len);
 		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 
 	system("pause");
